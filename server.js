3593e5a3ac9e57a081391dea3218d8d553f1b7a4
Dan Zaharia <zaharia.danny@gmail.com>
2025-06-27 16:04:39 -0400

    Have express server point to index.html

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const port = 3100;
 
+// Serve static files from the 'public' directory
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
 app.get('/', (req, res) => {
-  res.send('Hello world!');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
-
